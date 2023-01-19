e9e089d75360a1d810e1fa935dde9083f164d7d9
Fondueserv <116924265+Fondueserv@users.noreply.github.com>
2023-01-19 11:40:45 -0700

    Update tripwire.py
    replaced hashFile method with the updated version from hash.py

--- tripwire/tripwire.py
+++ tripwire/tripwire.py
@@ -52,27 +52,48 @@ def tripwireDir(directory, record, c):
         with open(record, "a") as file:
             file.write(changes_log_contents)
 
-#read file content and return a hash result using a hash algorithm
+#read target content and return a hash result using a hash algorithm
 # such as:    fileHash = hashlib.md5(data).hexdigest()import hashlib
+def hashFile():
+    target = input("What file/folder would you like to hash? ")
+    algorithm = input("which algorithm would you like to use for hashing? (sha256, sha1, md5) ")
+    # Open the file in read mode 
+           
+# check if the target input is a file or a directory   
 
-def hash_file(directory, algorithm):
-    # Open the file in read mode
-    with open(directory, "rb") as file:
-        # Read the contents of the file
-        file_contents = file.read()
-        
+# this handles the case where the target is a file:   
+    if  os.path.isfile(target): 
+        with open(target, 'rb') as f:
+            data = f.read()   
     # Create a new hash object using the specified algorithm
-    if algorithm == "sha256":
-        fileHash = hashlib.sha256()
-    elif algorithm == "md5":
-        fileHash = hashlib.md5()
-    elif algorithm == "sha1":
-        fileHash = hashlib.sha1()
-    else:
-        raise ValueError(f"Invalid algorithm: {algorithm}")
-    
-    # Update the hash object with the file's contents
-    fileHash.update(file_contents)
-    
-    # Return the hexadecimal representation of the hash object
-    return fileHash.hexdigest()
+        if algorithm == "sha256":
+            fileHash = hashlib.sha256(data)
+        elif algorithm == "md5":
+            fileHash = hashlib.md5(data)
+        elif algorithm == "sha1":
+            fileHash = hashlib.sha1(data)
+        else:
+            raise ValueError(f"Invalid algorithm: {algorithm}")
+        print(fileHash)
+# this handles the case where the target is a directory:
+    elif os.path.isdir(target):
+        for root, dirs, files in os.walk(target):
+            for file in files:
+                file_path = os.path.join(root, file)
+                with open(file_path, 'rb') as f:
+                    if algorithm == "sha256":
+                        fileHash = hashlib.sha256()
+                    elif algorithm == "md5":
+                        fileHash = hashlib.md5()
+                    elif algorithm == "sha1":
+                        fileHash = hashlib.sha1()
+                    else:
+                        raise ValueError(f"Invalid algorithm: {algorithm}")
+                    while True:
+                        data = f.read(1024)
+                        if not data:
+                            break
+                        fileHash.update(data)
+                    print(f'{file_path}: {fileHash.hexdigest()}')
+
+hashFile()
