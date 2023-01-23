f9bea0ea4afa34f456ef45e7ea231074d733fe09
Fondueserv <116924265+Fondueserv@users.noreply.github.com>
2023-01-23 13:47:58 -0700

    Update tripwire.py
    Last update didn't work. This one works. Will print a list of each file + hash in the given directory

--- tripwire/tripwire.py
+++ tripwire/tripwire.py
@@ -25,11 +25,14 @@ def tripwireDir():
     record = input("What is the name of the record file?  ")
     algorithm = input("which algorithm would you like to use for hashing? (sha256, sha1, md5) ")
     
-    #everything below handles the hashing of files: 
-    
+        #everything below handles the hashing of files: 
+
     def hashFile():
 
-        def hashing():
+    # this handles the case where the target is a file:   
+        if  os.path.isfile(target): 
+            with open(target, 'rb') as f:
+                data = f.read()   
         # Create a new hash object using the specified algorithm
             if algorithm == "sha256":
                 fileHash = hashlib.sha256(data)
@@ -39,33 +42,28 @@ def tripwireDir():
                 fileHash = hashlib.sha1(data)
             else:
                 raise ValueError(f"Invalid algorithm: {algorithm}")
-            while True:
-                data = f.read(1024)
-                if not data:
-                    break
-            fileHash.update(data)  
-            hashResult = fileHash.hexdigest()
-            return hashResult
-        
-    # this handles the case where the target is a file:   
-        if  os.path.isfile(target): 
-            with open(target, 'rb') as f:
-                hashing()  
+            print(fileHash.hexdigest())
     # this handles the case where the target is a directory:
         elif os.path.isdir(target):
             for root, dirs, files in os.walk(target):
                 for file in files:
                     file_path = os.path.join(root, file)
                     with open(file_path, 'rb') as f:
-                        hashing()    
-                               
-    for child in os.listdir(target):
-        child_path = os.path.join(target, child)
-        if os.path.isdir(child_path):
-            return(f"[{child}]")
-        else:
-            return(child)
-    print(child, hashFile.hashing())
+                        if algorithm == "sha256":
+                            fileHash = hashlib.sha256()
+                        elif algorithm == "md5":
+                            fileHash = hashlib.md5()
+                        elif algorithm == "sha1":
+                            fileHash = hashlib.sha1()
+                        else:
+                            raise ValueError(f"Invalid algorithm: {algorithm}")
+                        while True:
+                            data = f.read(1024)
+                            if not data:
+                                break
+                            fileHash.update(data)
+                        print(f'{file}: {fileHash.hexdigest()}')  
+    hashFile()
 #this is for creating a record file
 
     
